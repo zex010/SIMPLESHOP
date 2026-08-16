7d396eac41a3a2f0db11e06157194410cf859a6c
zex010 <ntf2502@gmail.com>
2026-08-16 21:48:14 +0500

    Fix image utility

--- client/src/utils/imageUtils.js
+++ client/src/utils/imageUtils.js
@@ -4,7 +4,11 @@
 export const resolveImage = (image) => {
   if (!image) return "";
 
-  if (image.startsWith("http://") || image.startsWith("https://")) {
+  if (
+    image.startsWith("http://") ||
+    image.startsWith("https://") ||
+    image.startsWith("data:")
+  ) {
     return image;
   }
 
@@ -16,7 +20,7 @@ export const resolveImage = (image) => {
 };
 
 export const getDisplayName = (product) => {
-  if (!product) return "";
+  if (!product) return "AVERNUS";
 
   return (
     product.name ||
